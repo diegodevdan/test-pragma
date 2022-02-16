9805df460ef4d9fde409f7abe120edff9c979bb7
diego-herrera-chairez <diego.herrera@schilleramericas.com>
2022-02-16 11:03:31 -0600

    first commit

---
+++ src/TestPragma.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPragmaApp = () => {
+    return (
+        <div>
+            <h1>Test pragma app</h1>
+        </div>
+    );
+};
+
+export default TestPragmaApp;
